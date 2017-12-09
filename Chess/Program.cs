a8c7d500af98e6cf0a7af53ac045882aee797384
hnefatafl01 <kevin.erick01@gmail.com>
2017-12-09 10:35:23 -0700

    typo

--- Chess/Program.cs
+++ Chess/Program.cs
@@ -5,6 +5,7 @@ namespace Chess
 {
     class Program
     {
+        public static Piece value = null;
         static void Main(string[] args)
         {
             Board board = new Board();
@@ -13,16 +14,18 @@ namespace Chess
             pawns.SetPieces("Pawn", pieces, board, 8);
 
             //Console.WriteLine($"color:{wp1.Color} x{wp1.Coords.X}, y{wp1.Coords.Y}", wp1.Color, wp1.Coords.X, wp1.Coords.Y);
-
             
+            pieces.TryGetValue("wp1", out value);
+
+            Console.WriteLine(value.Coords.X);
             //test print populated board
             //for (int i = 0; i < 8; i++) {
             //    for (int j = 0; j < 8; j++) {
             //      //Console.WriteLine(board.BoardLayout[i, j]);
-            //        if (board.BoardLayout[i ,j] == wp8.Coords) {
+            //        if (board.BoardLayout[i ,j] == pawns["wp1"]) {
             //            Console.WriteLine($"found wp1 {board.BoardLayout[i, j].X} {board.BoardLayout[i, j].Y}", board.BoardLayout[i, j].X, board.BoardLayout[i, j].Y);
             //        }
-            //        if (board.BoardLayout[i, j] == bp8.Coords)
+            //        if (board.BoardLayout[i, j] == pawns["bp8"].Coords)
             //        {
             //            Console.WriteLine($"found bp1 {board.BoardLayout[i, j].X} {board.BoardLayout[i, j].Y}", board.BoardLayout[i, j].X, board.BoardLayout[i, j].Y);
             //        }
